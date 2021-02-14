6385e32c73a790a3fed548c2fd6ac6c984d5de63
Aggelos Karapatakis <63917743+Karapatakis106@users.noreply.github.com>
2021-02-14 21:28:08 +0200

    Add files via upload

---
+++ anim.py
@@ -0,0 +1,15 @@
+import os
+from os import system, name
+from time import sleep  
+os.system('cls')   
+print ("************                         ")
+sleep(2)
+os.system('cls')
+print("")
+print ("                      ***************")
+print (" ")
+sleep(2)
+os.system('cls')
+print ("************                         ")
+print ("             Hello World!!! ")
+print ("                      ***************")
